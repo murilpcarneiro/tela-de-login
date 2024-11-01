015edc4ff5e480ef37680f4798b952e43839a494
Murilo <murilpcarneiro@gmail.com>
2024-11-01 12:29:17 -0300

    commit alteração dos links

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const email = document.getElementById('email');
 
 function login() {
   console.log(senha.value, email.value);
-  if (email.value === "admin@cesupa.br" && senha.value === "123123") {
-    window.location = "https://www.cesupa.br";
+  if (email.value === "pedro@giroto.com" && senha.value === "223344") {
+    window.location = "https://www.detran.pa.gov.br/index_.php";
   }
   else if (email.value === "alessandra@chefa.com" && senha.value === "1122") {
     window.location = "https://www.cesupa.br";
   }
-  else if (email.value === "isaacjava@oracle.com" && senha.value === "asdasd") {
-    window.location = "https://www.cesupa.br";
+  else if (email.value === "isaacjava@java.com" && senha.value === "asdasd") {
+    window.location = "https://en.idopterlabs.com.br";
   }
   else {
     document.write("erro");
