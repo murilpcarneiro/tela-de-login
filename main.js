495b2028d721160e9f4b48f1628a0fbbf14b4c6e
Murilo <murilpcarneiro@gmail.com>
2024-11-07 19:41:50 -0300

    commit final

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
 const senha = document.getElementById('senha');
 const email = document.getElementById('email');
+const erroEmail = document.getElementById('erro-email');
+const erroSenha = document.getElementById('erro-senha');
 
 function login() {
-  console.log(senha.value, email.value);
-  if (email.value === "pedro@giroto.com" && senha.value === "223344") {
+  console.log(`Email: ${email.value} Senha: ${senha.value}`);
+  if (email.value === "" && senha.value === "") {
+    erroEmail.innerHTML = "Preencha o campo email";
+    erroSenha.innerHTML = "Preencha o campo senha";
+  }
+  else if (email.value === "") {
+    erroEmail.innerHTML = "Preencha o campo email";
+    erroSenha.innerHTML = "";
+  }
+  else if (senha.value === "") {
+    erroEmail.innerHTML = "";
+    erroSenha.innerHTML = "Preencha o campo senha";
+  }
+  else if (email.value === "pedro@giroto.com" && senha.value === "223344") {
     window.location = "https://www.detran.pa.gov.br/index_.php";
   }
-  else if (email.value === "alessandra@chefa.com" && senha.value === "1122") {
+  else if (email.value !== "pedro@girotto.com" && senha.value === "223344") {
+    erroEmail.innerHTML = "Email incorreto";
+  }
+  else if (email.value === "pedro@giroto.com" && senha.value !== "223344") {
+    erroSenha.innerHTML = "Senha incorreta";
+  }
+  else if (email.value === "ale@cesupa.br" && senha.value === "123123") {
     window.location = "https://www.cesupa.br";
+  }
+  else if (email.value !== "ale@cesupa.br" && senha.value === "123123") {
+    erroEmail.innerHTML = "Email incorreto";
+  }
+  else if (email.value === "ale@cesupa.br" && senha.value !== "123123") {
+    erroSenha.innerHTML = "Senha incorreta";
   }
   else if (email.value === "isaacjava@java.com" && senha.value === "asdasd") {
     window.location = "https://en.idopterlabs.com.br";
   }
+  else if (email.value !== "isaac@java.com" && senha.value === "asdasd") {
+    erroEmail.innerHTML = "Email incorreto";
+  }
+  else if (email.value === "isaac@java.com" && senha.value !== "asdasd") {
+    erroSenha.innerHTML = "Senha incorreta";
+  }
   else {
-    document.write("erro");
+    erroEmail.innerHTML = "Email incorreto";
+    erroSenha.innerHTML = "Senha incorreta";
   }
 }
